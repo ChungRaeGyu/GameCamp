342f45c74b1318478e110151d5a7409f43924aee
Chung Rae Gyu <wogns9769@gmail.com>
2026-07-31 15:37:59 +0900

    데미지 입음

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,34 @@
 using System;
 using UnityEngine;
-
-public class Enemy : MonoBehaviour
+using UnityEngine.InputSystem.Processors;
+public interface Idamagable
+{
+    public void TakeDamaged(float damage);
+}
+public class Enemy : MonoBehaviour, Idamagable
 {
     // Update is called once per frame
     int index = 0;
     [SerializeField] private float speed = 1.0f;
+    float hp = 100;  //나중에 라운드당 Hp를 추가로 줄것이다.
+
+    public bool TakeDamaged(float damage)
+    {
+        hp = Mathf.Max(hp - damage, 0);
+
+        if(hp == 0)
+        {
+            Dead();
+            return true;
+        }
+        return false;
+    }
+
+    private void Dead()
+    {
+        Destroy(gameObject);
+    }
+
     void Update()
     {
         if (index >= GameManager.instance.waypoints.Length) return;
